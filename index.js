8dde71d0192d969fa707094670ab38f7e2f2d6b9
Juan Batiz-Benet <juan@benet.ai>
2014-10-17 14:39:24 -0700

    callback + streams interface.

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 var request = require('request');
+var through2 = require('through2');
+// accum is like concat-stream, but as an obj stream2.
+var accum = require('accum-transform');
 
-var getDataFromDoi = function(doi) {
-  if (doi === null) return;
-  var api = 'http://api.altmetric.com/v1/doi/';
+var api = 'http://api.altmetric.com/v1/doi/';
 
-  return request({
+var getDataFromDoi = function(doi, callback) {
+  if (doi === null)
+    cb(new Error("no doi"));
+
+  var reqOpts = {
     uri: api + doi,
     method: 'GET',
-    protocol: 'http:'
-  }, function (error, response, body) {
+    protocol: 'http:',
+    json: 'true'
+  }
+
+  return request(reqOpts, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      return body;
+      // body is already parsed (json:true above)
+      // need to call the callback we were given.
+      callback(null, body);
     }
   });
 };
 
+var getDataFromDoiStream = function(doi) {
+  if (doi === null)
+    throw new Error("no doi"); // throws because programmer error.
+
+  var reqStream = request({
+    uri: api + doi,
+    method: 'GET',
+    protocol: 'http:'
+  })
+
+  var parse = through2.obj(function (data, enc, done) {
+    this.push(JSON.parse(data))
+    done()
+  })
+
+  // when using pipe in requests, we get raw data. so need to parse it.
+  return reqStream
+    .pipe(accum())
+    .pipe(parse)
+}
+
 
 exports.getDataFromDoi = getDataFromDoi;
+exports.getDataFromDoiStream = getDataFromDoiStream;
